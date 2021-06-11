0912559ff173e90e3ecc65d334a4e32693a439b2
ubiquitouss <ubiquitouss@yahoo.com>
2021-06-11 11:33:19 +0600

    life cycle initially working

--- src/SeasonsDisplay.js
+++ src/SeasonsDisplay.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
 
-    return <div>Season Display</div>;
+const getSeason = (lat,month)=>{
+    if (month > 2 && month < 9) {
+        //if lat >  0 then we are in northern hemisphere, then there is summer
+        //in 3 to 8 means april to semtember
+       return lat > 0 ? 'summer' : 'winter';
+    } else {
+        //if lat > 0 then we are in northern hemisphere , else means it's winter
+        // without 3 to 8 means april to september
+        // we have 0 to 2 and 9 to 11 means january to march and october to december
+       return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+
+const SeasonDisplay = (props) => {
+    //this props is this.state.lat means just the latitude portion
+    //on northern hemisphere  summer is (april to september)
+    //on southern hemisphere summer is (jan to march) and (oct to dec)
+    //month range is 0 to 11
+    console.log(props.lat);
+    const season = getSeason(props.lat,new Date().getMonth())
+    console.log(season)
+    //props.lat = lattitude, new Date().getMonth() is javascript code for current month
+    return <div>Season Display</div>
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SeasonDisplay from './SeasonsDisplay'
+
 // const App = () => {
 //     window.navigator.geolocation.getCurrentPosition(
 //         //first line shows the location
@@ -16,43 +18,62 @@ import ReactDOM from 'react-dom';
 //     )
 // }
 
-
+//!after the updated state method the constructor has only one purpose
+//! to initiate the initial data
 class App extends React.Component {
     //right now i'm considering the state method as a javascript class as i've learned
-    constructor (props) {
-        super(props);
+    // constructor (props) {
+    //     super(props);
          
-        //! this is the only time we do direct assignment to this.state
-        this.state = {lat: null,errorMessage: ''};
-        //for now we don't know whats the number of lattitude
-        //that's why we've to use null initially
-        //if we use 40 replacing null . the value will show 40
+    //     //! this is the only time we do direct assignment to this.state
+    //     this.state = {lat: null,errorMessage: ''};
+    //     //for now we don't know whats the number of lattitude
+    //     //that's why we've to use null initially
+    //     //if we use 40 replacing null . the value will show 40
+    //      //?this is normal method
+    //     // window.navigator.geolocation.getCurrentPosition(
+    //     //     (position) => {
+    //     //         // to update state we have to call setState!!!!
+    //     //         this.setState({lat: position.coords.latitude})
+    //     //         //in console look at object position
+    //     //         // we are setting position.coords.latitude to the lat:
+    //     //         //instead of null
+    //     //         //!we did not!!!!!
+    //     //         //!this.state.lat = position.coords.latitude
+    //     //     },
+    //     //     (err) => {
+    //     //         console.log(err)//check this on the console to know err.message
+    //     //         this.setState({errorMessage: err.message});
+    //     //     }
+    //     // );
+
+    // }
+    //!this line works just like the constructor without the constructor
+    //! just like a class just think it like a class system
+    //! a variable outside class is accessable by everything
+    state = {lat: null, errorMessage:''}
+
+    componentDidMount() {
+       //?updated method
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // to update state we have to call setState!!!!
-                this.setState({lat: position.coords.latitude})
-                //in console look at object position
-                // we are setting position.coords.latitude to the lat:
-                //instead of null
-                //!we did not!!!!!
-                //!this.state.lat = position.coords.latitude
-            },
-            (err) => {
-                console.log(err)//check this on the console to know err.message
-                this.setState({errorMessage: err.message});
-            }
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errorMessage: err.message})
         );
     }
+
+
     render (){
            if (this.state.errorMessage && !this.state.lat) {
                return <div> Error: {this.state.errorMessage}</div>
             }
            if (!this.state.errorMessage && this.state.lat) {
-            return <div> Error: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
             }
             return <div>Loading......</div>
     }
 }
+
+
 
 
 ReactDOM.render(
